54eeaced29067221c339badbc47e49046e217e8b
Astware Developer <astwareincorporated@gmail.com>
2025-08-06 13:55:36 +0400

    Create circuits.js

---
+++ circuits.js
@@ -0,0 +1,9 @@
+const circuits = [
+  {
+    id: 1,
+    Stock: 0,
+    title: "Smart Breezer",
+    description: "idk what to tell but let this string exist as an placeholder, uhh the end.",
+    image: "/Product_1.png"
+  }
+]
